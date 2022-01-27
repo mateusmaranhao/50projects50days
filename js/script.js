d3e8c5cfcc869728c2d4bd6833e78dfde288cb9a
Mateus <mateusmaranhao0210@gmail.com>
2022-01-27 08:17:03 -0300

    adicionar Scroll Animation

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ function initSmoothScroll() {
   });
 }
 initSmoothScroll();
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length) {
+    const halfWindow = window.innerHeight * 0.67;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - halfWindow) < 0;
+
+        if(isSectionVisible) {
+          section.classList.add('active');
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll)
+  }
+}
+initScrollAnimation()
